8f6e480c5b7b191b9a9822b51437f74141a84e53
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-02-04 07:03:52 +0500

    add categories app and model

---
+++ blog_api/apps/categories/models.py
@@ -0,0 +1,12 @@
+from autoslug import AutoSlugField
+from django.db.models import CharField, Model
+
+
+class Category(Model):
+    name = CharField(max_length=100, null=False)
+    slug = AutoSlugField(populate_from="name", unique=True, null=False)
+
+    class Meta:
+        db_table = "categories"
+        verbose_name = "category"
+        verbose_name_plural = "categories"
--- blog_api/settings/base.py
+++ blog_api/settings/base.py
@@ -20,6 +20,7 @@ DJANGO_APPS = [
 ]
 PROJECT_APPS = [
     "apps.users.apps.UsersConfig",
+    "apps.categories.apps.CategoriesConfig",
 ]
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
