4435fee1110b75442aa9d34a2999aa88361382f5
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-02-13 13:03:06 +0500

    fix: use router to /users/ path to proper handle lookup_field

--- blog_api/apps/users/urls.py
+++ blog_api/apps/users/urls.py
@@ -3,9 +3,11 @@ from rest_framework.routers import DefaultRouter
 
 from .views import AvatarViewSet, UserViewSet
 
+router = DefaultRouter()
+router.register("", UserViewSet, basename="user")
 
 urlpatterns = [
     path("users/me/", UserViewSet.as_view({"patch": "partial_update"})),
     path("users/me/avatar/", AvatarViewSet.as_view({"patch": "partial_update"})),
-    path("users/", UserViewSet.as_view({"get": "list"})),
+    path("users/", include(router.urls)),
 ]
