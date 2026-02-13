b0811a318c1db20d24c81f26f54d97fdc42cf574
Scarlet Scarf <156634569+smileBlueRose@users.noreply.github.com>
2026-02-13 10:24:13 +0500

    add default pagination class to the project

--- blog_api/settings/base.py
+++ blog_api/settings/base.py
@@ -109,6 +109,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "EXCEPTION_HANDLER": "middleware.exception_handlers.custom_exception_handler",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 20,
 }
 
 
